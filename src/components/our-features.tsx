3cd9e78058a8a8471809cc76ce668fb42f0747da
rabiabaloch425 <rabiabaloch425@gmail.com>
2023-02-17 20:55:00 +0500

    features-card

--- src/components/our-features.tsx
+++ src/components/our-features.tsx
@@ -16,15 +16,17 @@ const Ourfeatures = () => {
             xs={12}
             md={6}
             lg={4}
-            className="mt-[6rem] mb-[2rem] sm:px-[3em] md:px-[3em] lg:px-[1em] xl:px-[6em]"
+            className="mt-[6rem] mb-[2rem] flex justify-center"
           >
-            <div className="bg-white rounded-2xl px-6 py-12 relative">
-              <img
-                src="../../public/assets/images/green-white.png"
-                alt="green-white"
-                className="absolute w-[157px] top-0 sm:left-[25%] md:left-[24%] translate-y-[-5em]"
-              />
-              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-16">
+            <div className="bg-white rounded-2xl px-6 py-12 relative w-[340px]">
+              <div className="flex justify-center">
+                <img
+                  src="../../public/assets/images/green-white.png"
+                  alt="green-white"
+                  className=" w-[157px] translate-y-[-7em]"
+                />
+              </div>
+              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-[-5em]">
                 الوصول إلى المستثمرين
               </h2>
               <h2 className="text-[#9CA6AF] sm:!text-[.9rem] md:!text-[.9rem]">
@@ -43,15 +45,17 @@ const Ourfeatures = () => {
             xs={12}
             md={6}
             lg={4}
-            className="mt-[6rem] mb-[2rem] sm:px-[3em] md:px-[3em] lg:px-[1em] xl:px-[6em]"
+            className="mt-[6rem] mb-[2rem] flex justify-center"
           >
-            <div className="bg-white rounded-2xl px-6 py-12 relative">
-              <img
-                src="../../public/assets/images/green-white.png"
-                alt="green-white"
-                className="absolute w-[157px] top-0 sm:left-[25%] md:left-[24%] translate-y-[-5em]"
-              />
-              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-16">
+            <div className="bg-white rounded-2xl px-6 py-12 relative w-[340px]">
+              <div className="flex justify-center">
+                <img
+                  src="../../public/assets/images/green-white.png"
+                  alt="green-white"
+                  className=" w-[157px] translate-y-[-7em]"
+                />
+              </div>
+              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-[-5em]">
                 الوصول إلى المستثمرين
               </h2>
               <h2 className="text-[#9CA6AF] sm:!text-[.9rem] md:!text-[.9rem]">
@@ -70,15 +74,17 @@ const Ourfeatures = () => {
             xs={12}
             md={6}
             lg={4}
-            className="mt-[6rem] mb-[2rem] sm:px-[3em] md:px-[3em] lg:px-[1em] xl:px-[6em]"
+            className="mt-[6rem] mb-[2rem] flex justify-center"
           >
-            <div className="bg-white rounded-2xl px-6 py-12 relative">
-              <img
-                src="../../public/assets/images/green-white.png"
-                alt="green-white"
-                className="absolute w-[157px] top-0 sm:left-[25%] md:left-[24%] translate-y-[-5em]"
-              />
-              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-16">
+            <div className="bg-white rounded-2xl px-6 py-12 relative w-[340px]">
+              <div className="flex justify-center">
+                <img
+                  src="../../public/assets/images/green-white.png"
+                  alt="green-white"
+                  className=" w-[157px] translate-y-[-7em]"
+                />
+              </div>
+              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-[-5em]">
                 الوصول إلى المستثمرين
               </h2>
               <h2 className="text-[#9CA6AF] sm:!text-[.9rem] md:!text-[.9rem]">
@@ -97,15 +103,17 @@ const Ourfeatures = () => {
             xs={12}
             md={6}
             lg={4}
-            className="mt-[6rem] mb-[2rem] sm:px-[3em] md:px-[3em] lg:px-[1em] xl:px-[6em]"
+            className="mt-[6rem] mb-[2rem] flex justify-center"
           >
-            <div className="bg-white rounded-2xl px-6 py-12 relative">
-              <img
-                src="../../public/assets/images/green-white.png"
-                alt="green-white"
-                className="absolute w-[157px] top-0 sm:left-[25%] md:left-[24%] translate-y-[-5em]"
-              />
-              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-16">
+            <div className="bg-white rounded-2xl px-6 py-12 relative w-[340px]">
+              <div className="flex justify-center">
+                <img
+                  src="../../public/assets/images/green-white.png"
+                  alt="green-white"
+                  className=" w-[157px] translate-y-[-7em]"
+                />
+              </div>
+              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-[-5em]">
                 الوصول إلى المستثمرين
               </h2>
               <h2 className="text-[#9CA6AF] sm:!text-[.9rem] md:!text-[.9rem]">
@@ -124,15 +132,17 @@ const Ourfeatures = () => {
             xs={12}
             md={6}
             lg={4}
-            className="mt-[6rem] mb-[2rem] sm:px-[3em] md:px-[3em] lg:px-[1em] xl:px-[6em]"
+            className="mt-[6rem] mb-[2rem] flex justify-center"
           >
-            <div className="bg-white rounded-2xl px-6 py-12 relative">
-              <img
-                src="../../public/assets/images/green-white.png"
-                alt="green-white"
-                className="absolute w-[157px] top-0 sm:left-[25%] md:left-[24%] translate-y-[-5em]"
-              />
-              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-16">
+            <div className="bg-white rounded-2xl px-6 py-12 relative w-[340px]">
+              <div className="flex justify-center">
+                <img
+                  src="../../public/assets/images/green-white.png"
+                  alt="green-white"
+                  className=" w-[157px] translate-y-[-7em]"
+                />
+              </div>
+              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-[-5em]">
                 الوصول إلى المستثمرين
               </h2>
               <h2 className="text-[#9CA6AF] sm:!text-[.9rem] md:!text-[.9rem]">
@@ -151,15 +161,17 @@ const Ourfeatures = () => {
             xs={12}
             md={6}
             lg={4}
-            className="mt-[6rem] mb-[2rem] sm:px-[3em] md:px-[3em] lg:px-[1em] xl:px-[6em]"
+            className="mt-[6rem] mb-[2rem] flex justify-center"
           >
-            <div className="bg-white rounded-2xl px-6 py-12 relative">
-              <img
-                src="../../public/assets/images/green-white.png"
-                alt="green-white"
-                className="absolute w-[157px] top-0 sm:left-[25%] md:left-[24%] translate-y-[-5em]"
-              />
-              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-16">
+            <div className="bg-white rounded-2xl px-6 py-12 relative w-[340px]">
+              <div className="flex justify-center">
+                <img
+                  src="../../public/assets/images/green-white.png"
+                  alt="green-white"
+                  className=" w-[157px] translate-y-[-7em]"
+                />
+              </div>
+              <h2 className="text-[#77838F] sm:!text-[.9rem] md:!text-[.9rem] mt-[-5em]">
                 الوصول إلى المستثمرين
               </h2>
               <h2 className="text-[#9CA6AF] sm:!text-[.9rem] md:!text-[.9rem]">
